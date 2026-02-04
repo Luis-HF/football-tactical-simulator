5f32af9b0d6ef08afdc2545272b9f85b6365c2e1
LUIS HENRIQUE <lhfspro@gmail.com>
2026-02-04 18:58:25 -0300

    feat: setup initial spring boot server structure

---
+++ server/src/test/java/com/example/back21/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
