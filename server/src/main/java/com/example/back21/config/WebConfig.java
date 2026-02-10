2b2ea5b7848c0ce7604977d777ef427bd91f4a2d
LUIS HENRIQUE <lhfspro@gmail.com>
2026-02-10 02:49:02 -0300

    feat/open CORS to python socket and localhost

---
+++ server/src/main/java/com/example/back21/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.back21.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5500",
+                        "http://100.116.54.101:5500"
+                )
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
